b6c367c29204b58c0bdfe0d5c5d029ee270501c4
MMA <58857539+TheGeeKing@users.noreply.github.com>
2024-12-02 12:12:43 +0100

    improve auth middleware response

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,13 @@ import "dotenv/config";
 
 export default (req, res, next) => {
   try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
     const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const { userId } = decodedToken;
     req.auth = {
